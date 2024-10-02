f72c93fd91c3eb7010d4083a6434437a2c2610b1
Ajay-Babare <ajaybabare97@gmail.com>
2024-10-02 19:11:58 +0530

    bubble sort

---
+++ src/basicSorting/bubbleSort/BubbleSort.java
@@ -0,0 +1,32 @@
+package basicSorting.bubbleSort;
+
+import java.util.Arrays;
+
+//TC = O(n^2)
+//SC = O(1)
+public class BubbleSort {
+		//bubble sort
+		public static void bubbleSort(int[] arr) {
+			for(int i=0;i<arr.length-1;i++) { // n-1 pass
+				boolean flag = true;
+				for(int j=0;j<arr.length-1-i;j++) {
+					if(arr[j]>arr[j+1]) {
+						int temp = arr[j];
+						arr[j] = arr[j+1];
+						arr[j+1] = temp;
+						flag = false;
+					}
+				}
+				if(flag)
+					break;
+			}
+		}
+	public static void main(String[] args) {
+		int[] arr = {5,2,4,3,1};
+		System.out.println("Before Sort: "+Arrays.toString(arr));
+		bubbleSort(arr);
+		System.out.println("After Sort: "+Arrays.toString(arr));
+		
+	}
+
+}
---
+++ src/basicSorting/bubbleSort/MoveZeroes.java
@@ -0,0 +1,34 @@
+//LeetCode 283. Move Zeroes
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//Note that you must do this in-place without making a copy of the array.
+
+package basicSorting.bubbleSort;
+//TC = O(n^2)
+//SC = O(1)
+import java.util.Arrays;
+
+public class MoveZeroes {
+	
+	public static void moveZeroes(int[] nums) {
+        int noz = 0;
+        for(int ele : nums){
+            if(ele == 0)
+                noz++;
+        }
+        for(int i=0; i<noz; i++){
+            for(int j=0; j<nums.length-1-i;j++){
+                if(nums[j]==0){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+	public static void main(String[] args) {
+		int[] arr = {5,1,0,4,2,0,6,};
+		moveZeroes(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
